c21963e5b045cd60de8faef91424518601572a2b
SharonnaEvick <evicks5202@student.faytechcc.edu>
2024-09-22 21:47:50 -0400

    Add files via upload
    first upload

---
+++ P1LAB1EVICK_SHARONNA.py
@@ -0,0 +1,14 @@
+# Sharonna Evick
+# Date:9/22/2024
+# Assignement:P1LAB1
+# Description: This assignmentrequest that user enter their first and last name
+#              and then displays a welcome message.
+
+# Get the user's first name
+first_name = input("Please enter your first name: ")
+
+# Get the user's last name
+last_name = input("Please enter your last name: ")
+
+# Print a welcome message
+print(f"Hello, {first_name} {last_name}, Welcome to CTI-110!")
