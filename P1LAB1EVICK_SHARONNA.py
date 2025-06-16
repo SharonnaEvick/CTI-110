17533d0a5aee2c333934c42c72b149ed211773e3
SharonnaEvick <evicks5202@student.faytechcc.edu>
2025-06-16 12:39:57 -0400

    P1LAB1EVICK_SHARONNA.py

--- P1LAB1EVICK_SHARONNA.py
+++ P1LAB1EVICK_SHARONNA.py
@@ -5,10 +5,10 @@
 #              and then displays a welcome message.
 
 # Get the user's first name
-first_name = input("Please enter your first name: ")
+first_name = input("Enter your first name: ")
 
 # Get the user's last name
-last_name = input("Please enter your last name: ")
+last_name = input("Enter your last name: ")
 
 # Print a welcome message
 print(f"Hello, {first_name} {last_name}, Welcome to CTI-110!")
