48fbe92e23007430e171dd1a84f9f512ddf73422
SharonnaEvick <evicks5202@student.faytechcc.edu>
2025-07-12 14:11:49 -0400

    Add files via upload

---
+++ P1HW1_EvickSharonna.py
@@ -0,0 +1,14 @@
+print("----Calculating Exponents----")
+print()  # Blank line
+base = int(input("Enter the interger as the base number: "))
+exponent = int(input("Enter the interger as the exponent: "))
+result = base ** exponent
+print(f"{base} raised to the power of {exponent} is {result}!!")
+print()  # Blank line
+print("----Addition and Subtraction----")
+num1 = int(input("Enter a starting interger: "))
+num2 = int(input("Enter a interger to add: "))
+num3 = int(input("Enter a interger to subtract: "))
+sum_result = num1 + num2
+final_result = sum_result - num3
+print(f"{num1} + {num2} - {num3} is equal to {final_result}")
